064337f2c5cb3cf9119d6f3e0f3cfca75648c9c6
david <davidlo176@gmail.com>
2020-04-18 01:04:26 -0500

    reducido sloc

--- main.c
+++ main.c
@@ -1,39 +1,39 @@
 #include "config.h"
-/* En el archivo definitions.h se encuentran las definiciones de
- * las estructuras de datos para:
- *      - Remapeo de teclas
- *      - Lanzamiento de scripts
- *      - Capas
- *
- * En el archivo config.h se encuentran las definiciones del usuario de:
- *      - Remapeo de teclas
- *      - Lanzamiento de scripts
- *      - Capas
- */
-/* ######## Como funciona el envio de teclas: ########
- * cada key_stroke esta compuesto por 3 eventos
- * un evento per se envuelto entre dos eventos especiales
- *
- *      (Inicio envoltorio) ---------------> rap1
- *
- *                                 +-------> (evento press)
- *                                 |               
- *                  (event)  -> ---+-------> (evento release)
- *                                 |               
- *                                 +-------> (evento repeat)
- *
- *      (Fin envoltorio) ------------------> rap2
- *
- * ######## Como funciona una estructura input_event ########
- * Una estructura input_event está compuesta (para nuestro proposito)
- * de los siguientes valores
- *
- * input event{
- *      type  = tipo de evento (cuando es un tecleo: type=EV_KEY)
- *      code  = numero de la tecla presionada: (code = 30) -> KEY_A
- *      value = tipo evento de tecla 0=soltada 1=presionada 2=mantenida
- * }
- */
+// En el archivo definitions.h se encuentran las definiciones de
+// las estructuras de datos para:
+//      - Remapeo de teclas
+//      - Lanzamiento de scripts
+//      - Capas
+//
+// En el archivo config.h se encuentran las definiciones del usuario de:
+//      - Remapeo de teclas
+//      - Lanzamiento de scripts
+//      - Capas
+//
+// ######## Como funciona el envio de teclas: ########
+// cada key_stroke esta compuesto por 3 eventos
+// un evento per se envuelto entre dos eventos especiales
+//
+//      (Inicio envoltorio) ---------------> rap1
+//
+//                                 +-------> (evento press)
+//                                 |               
+//                  (event)  -> ---+-------> (evento release)
+//                                 |               
+//                                 +-------> (evento repeat)
+//
+//      (Fin envoltorio) ------------------> rap2
+//
+// ######## Como funciona una estructura input_event ########
+// Una estructura input_event está compuesta (para nuestro proposito)
+// de los siguientes valores
+//
+// input event{
+//      type  = tipo de evento (cuando es un tecleo: type=EV_KEY)
+//      code  = numero de la tecla presionada: (code = 30) -> KEY_A
+//      value = tipo evento de tecla 0=soltada 1=presionada 2=mantenida
+// }
+//
 
 int main(){
     // ruta absoluta al teclado que se va a usar
