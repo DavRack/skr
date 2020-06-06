d84527b08686f6006c206e136aa803b8eb29ceba
david <davidlo176@gmail.com>
2020-06-06 01:35:58 -0500

    added option to create empty config file

--- src/parser/parser.py
+++ src/parser/parser.py
@@ -13,4 +13,15 @@ if os.path.exists(configPath):
     for line in finalLines:
         print(line)
 else:
-    print("error file dont exist")
+    print("can't find config file in ~/.config/skr/skr.config")
+    print("create a blanck config file?")
+    
+    userAnswer = input("yes/no ")
+    userAnswer = userAnswer.upper()
+
+    if userAnswer == "YES" or userAnswer == "Y":
+        path = os.path.expanduser("~/.config/skr") 
+        os.makedirs(path)
+        path = path+"/skr.config"
+        os.mknod(path)
+        print("config file created")
