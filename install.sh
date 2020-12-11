19d9716d677670d5b46e37f777ab0fa45f85e911
David <davidlo176@gmail.com>
2020-12-10 21:47:43 -0500

    separate build and install functions

--- install.sh
+++ install.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 
 install(){
-    gcc src/core/main.c -o skrcore
 
     sudo mkdir -p /usr/share/skr 
     sudo cp -r src/parser /usr/share/skr
 
-    gcc src/kid/skrkid.c -o skrkid
     sudo cp skrkid /usr/share/skr
 
     sudo cp skrcore /usr/share/skr
     sudo cp skr /usr/bin/
+}
+build(){
+    gcc src/core/main.c -o skrcore
+    gcc src/kid/skrkid.c -o skrkid
 }
 uninstall(){
     sudo rm -rf /usr/share/skr
@@ -19,12 +21,21 @@ uninstall(){
 usage(){
     echo usage:
     echo " "
-    echo "      ./install.sh      installs skr" 
+    echo "      ./install.sh      builds and installs skr" 
+    echo "      ./install.sh -b   builds skr" 
+    echo "      ./install.sh -i   installs skr" 
     echo "      ./install.sh -u   uninstalls skr"
 }
 
 if [ "$#" = "0" ];then
     echo installing skr
+    build
+    install
+elif [ "$1" = "-b" ];then
+    echo building skr
+    build
+elif [ "$1" = "-i" ];then
+    echo building skr
     install
 
 elif [ "$1" = "-u" ];then
